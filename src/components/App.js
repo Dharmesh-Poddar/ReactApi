884d596b35e6ebd8f9a477a2ab3401cf88467fc6
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-05-16 22:46:35 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
 
 
 class App extends React.Component
@@ -30,7 +32,8 @@ class App extends React.Component
       
        
        <SearchBar onSubmit={this.onSearchSubmit} />
-        Found :{this.state.images.length} images
+       <ImageList images={this.state.images}/>
+
        </div>
        
         );
